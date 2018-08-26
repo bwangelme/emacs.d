479de83a1424d6ba269b20991a3ee6c6fd78a36b
xuyundong <xuyundong@douban.com>
2018-08-26 11:41:21 +0800

    update on 2018-08-26 11:41:21

---
+++ lisp/init-generic.el
@@ -0,0 +1,7 @@
+;; 关闭自动备份
+
+(setq make-backup-files nil) ; stop creating backup~ files
+(setq auto-save-default nil) ; stop creating #autosave# files
+(setq display-line-numbers-mode t) ; 显示行号
+
+(provide 'init-generic)
